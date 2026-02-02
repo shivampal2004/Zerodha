c82e01bc822770ccc090cfb8896c83263525f28d
Shivam Kumar <shivampal2004@gmail.com>
2026-02-03 00:07:43 +0530

    Added Positions Data

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
-const express= require("express");
-const app= express();
-app.listen(8080, ()=>{
-    console.log("App Started Listening on Port 8080")
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 8080;
+const url = process.env.MONGO_URL;
+const app = express();
+
+
+app.listen(PORT, () => {
+    console.log("App Started Listening on Port 8080");
+    mongoose.connect(url);
+    console.log("DB Connected");
 })
